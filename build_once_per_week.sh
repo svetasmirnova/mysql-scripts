1777bee3bd00890098e8445469fa8ffad3195bbf
Sveta Smirnova <sveta@js-client.com>
2019-05-22 01:42:14 +0300

    Added version 8.0 to Percona Server build script

--- build_once_per_week.sh
+++ build_once_per_week.sh
@@ -14,6 +14,6 @@ if [ ! -f "$log_dir/$last_build_filename" ] || [ `find "$log_dir/$last_build_fil
 then
 	touch "$log_dir/$last_build_filename"
 	"$BASEDIR/build.sh" >"$log_dir/${log_base_name}-mysql-"`date +%Y-%m-%d-%H-%M`.log 2>"$log_dir/${log_base_name}-mysql-"`date +%Y-%m-%d-%H-%M`.err
-	"$BASEDIR/build.sh" -g percona-server -p "ps-" -b 5.5 -b 5.6 -b 5.7 >"$log_dir/${log_base_name}-ps-"`date +%Y-%m-%d-%H-%M`.log 2>"$log_dir/${log_base_name}-ps-"`date +%Y-%m-%d-%H-%M`.err 
+	"$BASEDIR/build.sh" -g percona-server -p "ps-" -b 5.5 -b 5.6 -b 5.7 -b 8.0 >"$log_dir/${log_base_name}-ps-"`date +%Y-%m-%d-%H-%M`.log 2>"$log_dir/${log_base_name}-ps-"`date +%Y-%m-%d-%H-%M`.err 
 fi
 
